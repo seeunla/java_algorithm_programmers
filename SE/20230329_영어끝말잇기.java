2f982ccfe81e3e2b9ad916b6632bc25ad1f92ed0
김세은 <107592967+seeunla@users.noreply.github.com>
2023-03-30 16:22:48 +0900

    Update 20230329_영어끝말잇기.java

--- SE/20230329_영어끝말잇기.java
+++ SE/20230329_영어끝말잇기.java
@@ -1,109 +1,51 @@
 import java.util.*;
 
 class Solution {
-//     public int[] solution(int n, String[] words) {
-//         int[] answer = {0,0};
-//         int stop = 0;
-//         Stack<Character> st = new Stack<>();
-//         List<String> l = new ArrayList<>();
-        
-//         for (int i =0; i<words.length; i++) {
-//             char start = words[i].charAt(0);
-//             char end = words[i].charAt(words[i].length() -1);
-//             // System.out.println(start + " " + end);
-//             if (words[i].length() < 2 || words[i].length() >50) {
-//                 stop = i;
-//                 break;
-//             }
-//             if (st.isEmpty()) {
-//                 st.push(end);
-//                 l.add(words[i]);
-//             } else {
-//                 char piece = st.peek();
-//                 if (start == piece) {
-//                     st.pop();
-//                     st.push(end);
-//                 } 
-//                 if (start != piece) {
-//                     stop = i+1;
-//                     break;
-//                 }
-//                 if (l.contains(words[i])) {
-//                     stop = i+1;
-//                     break;
-//                 }
-//                 l.add(words[i]); 
-//             }
-//         }
+        public int[] solution(int n, String[] words) {
+                int[] answer = {0,0};
+                int stop = 0;
 
-//         if (stop % n != 0) {
-//                 answer[0] = stop % n;
-//                 answer[1] = stop/n + stop % n;
-//         } else if (stop -1 == words.length) {
-//             answer[0] = 0;
-//             answer[1] = 0;
-//         } 
-//         else {
-//             answer[0] = stop/ n;
-//             answer[1] = stop/n ;
-//         }
-        
-//         // System.out.println(stop);
+                List<String> l = new ArrayList<>();
 
-//         return answer;
-//     }
-    public int[] solution(int n, String[] words) {
-        int[] answer = {0,0};
-        int stop = 0;
-        
-        List<String> l = new ArrayList<>();
-        
-        for (int i =0; i<words.length; i++) {
-            if (l.size() == 0) {
-                l.add(words[i]);
-                continue;
-            }
-            if (l.contains(words[i])) {
-                stop = i+1;
-                break;
-            }
-            if (words[i].length() < 2 || words[i].length() >50) {
-                stop = i+1;
-                break;
-            }
-            if (words[i].length() == 1) {
-                stop = i+1;
-                break;
-            }
-            char start = words[i].charAt(0);
-            char end = l.get(i-1).charAt(l.get(i-1).length() -1);
+                for (int i =0; i<words.length; i++) {
+                    if (l.size() == 0) {
+                        l.add(words[i]);
+                        continue;
+                    }
+                    if (l.contains(words[i])) {
+                        stop = i;
+                        break;
+                    }
+                    if (words[i].length() < 2 || words[i].length() >50 
+                       || words[i].length() == 1) {
+                        stop = i;
+                        break;
+                    }
+                    if (words[i].length() == 1) {
+                        stop = i;
+                        break;
+                    }
+                    char start = words[i].charAt(0);
+                    char end = l.get(i-1).charAt(l.get(i-1).length() -1);
 
-            // System.out.println(start + " " + end);
+                    // System.out.println(start + " " + end);
 
-            if (start != end) {
-                stop = i+1;
-                break;
-            }
-            
-            l.add(words[i]);
+                    if (start != end) {
+                        stop = i;
+                        break;
+                    }
+                    l.add(words[i]);
+                }
 
-        }
+                // System.out.println(stop);
+                if (stop > 0) {
+                    answer[0] = stop % n +1;
+                    answer[1] = stop/n + 1;
+                    return answer;
+                } 
 
-        // System.out.println(stop);
-        if (stop % n != 0) {
-            answer[0] = stop % n;
-            answer[1] = stop/n + 1;
-            return answer;
-        } else if (stop % n == 0) {
-            answer[0] = stop/ n;
-            answer[1] = stop/n;
-            return answer;
-        } else if (l.size() == n) {
-            return answer;
-        }
-        
-        // System.out.println(l);
+                // System.out.println(l);
 
-        return answer;
-    }
+                return answer;
+         }
 }
